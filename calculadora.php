f0789e3a0381d6f679b0864fd02042a11190f5b7
Mrocanw01f <miguelcanedoads@gmail.com>
2023-06-23 08:59:45 -0300

    atualizacao

--- calculadora.php
+++ calculadora.php
@@ -5,22 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mercearia do João</title>
-    <!-- <link rel="stylesheet" href="css\bootstrap.min.css"> -->
     <link href="calculadora.css" rel="stylesheet">
     <link rel="icon" href="img/logo1.png">
-    
 </head>
 <body>
+
+
     <div class="exit">
     <a href="exit.php"><img src="img/remove.png" title="voltar" width="25" height="25"></a>
     </div>
-    <div class="btn-group" role="group" aria-label="..."><a href="calculadora.php"><id>Calculadora</id></a></div>
-    <div class="btn-group btn-group-sm" role="group" aria-label="..."><a href="sobre.php"><id>Sobre nós</id></a></div>
-    <br>
-    <br>
+    <id>Calculadora</id>
+    <a href="sobre.php"div class="sobre">Sobre</a>
     <figure><img src="img/calculadora.png" alt="calculos" width="150" height="150"></figure>
-    <br>
-    <br>
+    <h1><p>Valores</p></h1>
+    <form action= "calculadora.php" method= "post">
     <input class="fatores" type="number" name="num1" placeholder="n° 1" cols="50" required>
     <br>
     <br>
@@ -35,12 +33,14 @@
                 <td><input type="radio" name="operacoes" value="div">
                     <label>Divisão</label></td>
 
+
             </tr>
             <tr>
                 <td><input type="radio" name="operacoes" value="soma">
                     <label>Adição</label></td>
                 <td><input type="radio" name="operacoes" value="menos">
                     <label>Subtração</label></h2></td>
+
 
             </tr>
         </table>
@@ -51,7 +51,9 @@
         <textarea id="texto" type="text" style="resize:none" readonly rows= "2" cols="45">
         <?php
 
+
     session_start();
+
 
     //verifica se a sessão é existente
     if (!isset($_SESSION["usuário"])) {
@@ -62,7 +64,7 @@
             $num2 = ($_POST["num2"]);
             $operacoes = ($_POST["operacoes"]);
             $resultado = 0;
-        
+       
             if ($operacoes == "mult") {
                 $resultado = $num1 * $num2;
                 echo $resultado;
@@ -78,7 +80,7 @@
             elseif ($operacoes == "menos") {
                 $resultado = $num1 - $num2;
                 echo $resultado;
-            }   
+            }  
     }
             ?>
             </textarea>
@@ -86,8 +88,6 @@
 </form>
     <figure><img src="img/logo1.png" width="200" height="200"></figure>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 </html>
